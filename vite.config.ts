13146d4e1e55ef43940af4c732f286aa9ee8c535
samuelmccarthy16 <78646701+samuelmccarthy16@users.noreply.github.com>
2025-12-14 22:49:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// vite.config.ts
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/blissful/', // matches your GitHub repo name
-  // other configs...
-});
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/blissful/", // your GitHub repo name
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react()
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
